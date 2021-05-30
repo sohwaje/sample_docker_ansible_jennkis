22685a3b774fd500ff3e22eb2f9e801e2c1352b1
sohwaje <kissenyo@gmail.com>
2021-05-30 14:58:55 +0900

    update

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ COPY ./target/*.war app.war
 ## TimeZone 설정
 ENV TZ=Asia/Seoul
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-RUN apk update && apk add curl && mkdir /logs && mkdir /data_istool
+RUN apk update && apk add curl && mkdir /logs
 
 ## app Start
 # CMD ["java", "-jar", "-Dspring.profiles.active=stage", "-XX:MaxMetaspaceSize=512m", "-XX:MetaspaceSize=256m", "-Xms2048m", "-Xmx2048m", "/app.jar"]
---
+++ src/main/java/com/docker/example/hello/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.docker.example.hello.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  @RequestMapping("/")
+  public String hello() {
+    return "hello admin";
+  }
+}
